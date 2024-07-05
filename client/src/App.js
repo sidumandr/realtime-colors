e12514ebbac785975a9dac48a025fb57f17fc0f6
sidumandr <dumansafailhan@gmail.com>
2024-07-05 19:54:38 +0300

    real time colors with backend

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import "./App.css";
+import Palatte from "./components/Palatte";
+import { init, subscribe } from "./socketApi";
+import { useEffect, useState } from "react";
+
+function App() {
+  const [activeColor, setActiveColor] = useState("#282c34");
+
+  useEffect(() => {
+    init();
+    subscribe((color) => {
+      setActiveColor(color);
+    });
+  }, []);
+
+  return (
+    <div className="App" style={{ backgroundColor: activeColor }}>
+      <h1>{activeColor}</h1>
+      <Palatte activeColor={activeColor} />
+    </div>
+  );
+}
+
+export default App;
